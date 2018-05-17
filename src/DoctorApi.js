b5f4315418f074cf234778ff74e194299f9c008e
Michael Hollenberg <michael.hollenberg@uconn.edu>
2018-05-17 11:56:19 -0400

    docc api updated

--- src/DoctorApi.js
+++ src/DoctorApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASEURL = "https://www.omdbapi.com/?t=";
+const BASEURL = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=';
 const APIKEY = "144db4638bf7ea5ea76c0d3a06ac7ef1";
 
 export default {
@@ -7,3 +7,5 @@ export default {
     return axios.get(BASEURL + query + APIKEY);
   }
 };
+
+
